b93d9cc9ff0a49c6ca6feb1b535bb8eb2554ac16
Jaydan Urwin <jaydan@jaydanurwin.com>
2023-02-21 20:17:10 -0700

    settings url

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -1,18 +1,24 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { structure } from "./src/structure";
 
 export default defineConfig({
-  name: 'default',
-  title: 'Sanity Crash Course',
+  name: "default",
+  title: "Sanity Crash Course",
 
-  projectId: '0iczfznl',
-  dataset: 'production',
+  projectId: "0iczfznl",
+  dataset: "production",
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(
+      { structure },
+    ),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
